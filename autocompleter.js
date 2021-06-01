8b0bb5e555aec1e81e174a037f1ec49825487dfc
Zuzanna877 <79910579+Zuzanna877@users.noreply.github.com>
2021-06-01 10:54:52 +0200

    Add files via upload

---
+++ autocompleter.js
@@ -0,0 +1,132 @@
+Vue.component('v-autocompleter', {
+    template: `
+    <div class="vue-autocompleter">
+    <input
+      ref="first"
+      :value="value"
+      type="text"
+      class="search_input"
+      @input="$emit('input', $event.target.value)"
+      @keyup.down="goDown"
+      @keyup.up="goUp"
+      @keyup.enter="clickEnter" />
+      <div class="cities">
+        <li v-for="(city, index) in filteredCities" v-on:click="handleClick(city.name)" :class="{HighBack: index == forPick}">
+          <div class="podpowiedzi" v-on:click="choose(index)" v-html="highlight(city.name)"></div>
+        </li>
+      </div>
+    </div>`,
+    props: ['value', 'options'],
+    /**
+     * dzięki "value" przekazujemy naszą frazę, zaś "options" to nasza tworzona lista
+     */
+    data: function(){
+        return {
+            selected_city: '',
+            googleSearch_temp: '',
+            updated: true,
+            isActive: 0,
+            cities: window.cities,
+            forPick: -1,
+            filteredCities: []
+        }
+    },
+    watch: {
+      /**
+       * dzięki tej funkcji możemy przechodzić po liście
+       */
+        forPick: function () {
+            this.updated = false;
+            
+            if (this.forPick >= 0) {
+                this.$emit('input', this.filteredCities[this.forPick].name);
+              }
+            },
+        value: function(){
+            if(this.value.length == 0){
+                this.filteredCities = [];
+              } else{
+                this.updated=true;
+        
+                if(this.forPick == -1){
+                  this.googleSearch_temp = this.value; 
+                  this.CreateCities();     
+                }
+              }
+            }
+        },
+    methods: {
+      /**
+       * funkcja, która pokazuje nam 10 miast, które zawierają wpisaną frazę
+       */
+      CreateCities(){
+          let result = this.cities.filter(city => city.name.includes(this.value));
+          if(result.length > 10){
+            this.filteredCities = result.slice(1, 11);
+          }
+          else{
+            this.filteredCities = result;
+          }
+        this.forPick = -1;
+    },
+    /**
+     * Wystawienie eventu, po kliknięciu
+     * @param {miasto} name 
+     */
+      handleClick(name) {
+        this.$emit('input', this.value);
+        this.clickEnter();
+      },
+      /**
+       * Wystawienie eventu po wybraniu miasta o indeksie "i" z listy
+       * @param {indeks} i 
+       */
+      choose(i){
+        this.$emit('input', this.filteredCities[i].name);
+    },
+    /**
+     * pogrubienie tekstu, który nie został wpisany
+     * @param {fraza} phrase 
+     * @returns 
+     */
+      highlight: function(phrase) {
+        return phrase.replaceAll(this.value, '<span class="highlight">' + this.value + '</span>')
+      },
+      /**
+       * funkacja obsługuje przechodzenie w dół po liście
+       */
+      goDown(){
+        if(this.forPick < this.filteredCities.length -1){
+            this.forPick +=1;
+        }
+        else if(this.forPick == this.filteredCities.length -1){
+            this.forPick = -1;
+        }
+        },
+        /**
+         * przechodzenie w górę po liście
+         */
+      goUp(){
+       if(this.forPick > -1){
+           this.forPick -= 1;
+       }
+       else if(this.forPick == 0){
+           this.forPick = this.filteredCities.length -1;
+       }
+      },
+      /**
+       * Po naciśnięciu enter, wystawiany jest event po wybraniu miasta z listy
+       * @param {event} event 
+       */
+      clickEnter: function(event){
+        if(event) {
+            this.CreateCities();
+            this.forPick = -1;
+          }
+          this.$emit('enter', this.value);
+          this.isActive = 1;
+      }
+    },
+
+
+})
